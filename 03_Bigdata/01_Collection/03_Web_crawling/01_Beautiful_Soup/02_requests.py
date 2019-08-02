c3d4fdcdebc958c0a0cf0ae187386754f8d6d196
MSDN-SPECIAL <galma94815@naver.com>
2019-08-02 17:24:56 +0900

    업데이트

---
+++ 03_Bigdata/01_Collection/03_Web_crawling/01_Beautiful_Soup/02_requests.py
@@ -0,0 +1,27 @@
+import requests # 웹 페이지의 HTML을 가져오는 모듈
+from bs4 import BeautifulSoup # HTML을 파싱하는 모듈
+
+# 웹 페이지를 가져온 뒤 BeautigulSoup 객체로 만듦
+response = requests.get('http://www.weather.go.kr/weather/observation/currentweather.jsp')
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# <table class = "table_develop3">을 찾음
+table = soup.find('table', {'class': 'table_develop3'})
+data = []
+
+def data_correction(org_text):
+    if org_text == '\xa0':
+        return 'N/A'
+    return org_text
+
+for tr in table.find_all('tr'):
+    tds = list(tr.find_all('td'))
+    for td in tds:
+        if td.find('a'):
+            point = data_correction(td.find('a').text)
+            cloud = data_correction(tds[1].text)
+            visibility = data_correction(tds[2].text)
+            temperature = data_correction(tds[5].text)
+            wd_temp = data_correction(tds[7].text)
+            data.append([point, cloud, visibility, temperature, wd_temp])
+print(data)
